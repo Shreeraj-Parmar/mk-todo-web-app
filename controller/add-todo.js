5fb23e03bcb6de590af9cb6aa0c4d10a1a6b0988
Shreeraj-Parmar <mrobroie@yahoo.com>
2025-01-19 11:58:50 +0530

    :pencil: new design added

--- controller/add-todo.js
+++ controller/add-todo.js
@@ -1,75 +1,77 @@
 import { totaltask } from "./index.js";
 
-const task = document.querySelector('#addtext');// inputtask for task add 
-const taskdis = document.querySelector('#detail');//  ul tag
-let panding = document.querySelector('.panding');// panding counts.
+const task = document.querySelector("#addtext"); // inputtask for task add
+const taskdis = document.querySelector("#detail"); //  ul tag
+let panding = document.querySelector(".panding"); // panding counts.
 
 //that function used for new task added.
-const taskadd = ()=>{
-    if (task.value !== ''){
-        
-        const taskpera = document.createElement('li');
-        const taskdiv  = document.createElement('div');
-        
-        const editbtn = document.createElement('button');
-        const delbtn = document.createElement('button');
-        const complatebtn = document.createElement('button');
-        const importntbtn = document.createElement('button');
-        
-        delbtn.innerText = 'delete';
-        editbtn.innerText = 'edit';
-        complatebtn.innerText = 'complate';
-        importntbtn.innerText = 'imp';
-        
-        taskdiv.classList.add('pera');
-        taskpera.classList.add('p');
-        delbtn.classList.add('btnsize');
-        editbtn.classList.add('btnsize');
-        complatebtn.classList.add('btnsize');
-        importntbtn.classList.add('btnsize');
-        
-        delbtn.setAttribute('data-action','delete');
-        editbtn.setAttribute('data-action', 'edit');
-        complatebtn.setAttribute('data-action', 'complate');
-        importntbtn.setAttribute('data-action', 'impbtn');
-        
-        let taskid =Math.floor(Math.random()*Math.pow(9,5));
-        let dataTask = {
-            id:taskid,
-            task:task.value,
-            complatetask : false,
-            import : false
-        }
+const taskadd = () => {
+  if (localStorage.getItem("task") === null) {
+    localStorage.setItem("task", []);
+  }
 
-        totaltask.push(dataTask);
-        
-        taskpera.setAttribute('data-id', dataTask.id);
-        taskpera.setAttribute('status-id','panding')
+  if (task.value !== "") {
+    const taskpera = document.createElement("li");
+    const taskdiv = document.createElement("div");
 
-        localStorage.setItem('task',JSON.stringify(totaltask));
-        
-        let data =JSON.parse(localStorage.getItem('task'));
-        let maintask = data.find(task=>task.id === taskid);
+    const editbtn = document.createElement("button");
+    const delbtn = document.createElement("button");
+    const complatebtn = document.createElement("button");
+    const importntbtn = document.createElement("button");
 
-        taskpera.innerText = maintask.task;
-        
-        taskdis.appendChild(taskdiv);                                         
-        taskdiv.appendChild(taskpera);                                         
-        taskdiv.appendChild(editbtn);
-        taskdiv.appendChild(importntbtn);
-        taskdiv.appendChild(complatebtn);
-        taskdiv.appendChild(delbtn);
-        
-        task.value = '';
-        task.focus();
-        let uncomlatetask = totaltask.filter(task => task.complatetask === false);
-        panding.innerText = `Panding task is ${uncomlatetask.length}`;
-        }
-    else {
-        alert('please enter your task');
-        return false;
-    }
-}
+    delbtn.innerText = "delete";
+    editbtn.innerText = "edit";
+    complatebtn.innerText = "complate";
+    importntbtn.innerText = "imp";
 
-export{taskadd };
-export{task ,panding };
+    taskdiv.classList.add("pera");
+    taskpera.classList.add("p");
+    delbtn.classList.add("btnsize");
+    editbtn.classList.add("btnsize");
+    complatebtn.classList.add("btnsize");
+    importntbtn.classList.add("btnsize");
+
+    delbtn.setAttribute("data-action", "delete");
+    editbtn.setAttribute("data-action", "edit");
+    complatebtn.setAttribute("data-action", "complate");
+    importntbtn.setAttribute("data-action", "impbtn");
+
+    let taskid = Math.floor(Math.random() * Math.pow(9, 5));
+    let dataTask = {
+      id: taskid,
+      task: task.value,
+      complatetask: false,
+      import: false,
+    };
+
+    totaltask.push(dataTask);
+
+    taskpera.setAttribute("data-id", dataTask.id);
+    taskpera.setAttribute("status-id", "panding");
+
+    localStorage.setItem("task", JSON.stringify(totaltask));
+
+    let data = JSON.parse(localStorage.getItem("task"));
+    let maintask = data.find((task) => task.id === taskid);
+
+    taskpera.innerText = maintask.task;
+
+    taskdis.appendChild(taskdiv);
+    taskdiv.appendChild(taskpera);
+    taskdiv.appendChild(editbtn);
+    taskdiv.appendChild(importntbtn);
+    taskdiv.appendChild(complatebtn);
+    taskdiv.appendChild(delbtn);
+
+    task.value = "";
+    task.focus();
+    let uncomlatetask = totaltask.filter((task) => task.complatetask === false);
+    panding.innerText = `Panding task is ${uncomlatetask.length}`;
+  } else {
+    alert("please enter your task");
+    return false;
+  }
+};
+
+export { taskadd };
+export { task, panding };
--- controller/index.js
+++ controller/index.js
@@ -1,115 +1,110 @@
 import { taskadd } from "./add-todo.js";
-import { mainfunction } from"./mainfunction.js";
+import { mainfunction } from "./mainfunction.js";
 import { panding } from "./add-todo.js";
-import { complaTeTask,impTasktext } from "./mainfunction.js";
+import { complaTeTask, impTasktext } from "./mainfunction.js";
 
 let totaltask = [];
-const taskbtn = document.querySelector('#added');
-const todolist = document.querySelector('ul');
-const srachBtn = document.querySelector('#search');
-const taskdis = document.querySelector('#detail');
+const taskbtn = document.querySelector("#added");
+const todolist = document.querySelector("ul");
+const srachBtn = document.querySelector("#search");
+const taskdis = document.querySelector("#detail");
 
+document.addEventListener("DOMContentLoaded", () => {
+  totaltask = JSON.parse(localStorage.getItem("task"));
 
-document.addEventListener('DOMContentLoaded',()=>{
-    totaltask = JSON.parse(localStorage.getItem('task'));
-    
+  totaltask.forEach((element) => {
+    const taskpera = document.createElement("li");
+    const taskdiv = document.createElement("div");
+    const editbtn = document.createElement("button");
+    const delbtn = document.createElement("button");
+    const complatebtn = document.createElement("button");
+    const importntbtn = document.createElement("button");
 
-    totaltask.forEach(element => {
+    taskdiv.classList.add("pera");
+    taskpera.classList.add("p");
+    delbtn.classList.add("btnsize");
+    editbtn.classList.add("btnsize");
+    complatebtn.classList.add("btnsize");
+    importntbtn.classList.add("btnsize");
 
-        const taskpera = document.createElement('li');
-        const taskdiv  = document.createElement('div');
-        const editbtn = document.createElement('button');
-        const delbtn = document.createElement('button');
-        const complatebtn = document.createElement('button');
-        const importntbtn = document.createElement('button');
-    
-        taskdiv.classList.add('pera');
-        taskpera.classList.add('p');
-        delbtn.classList.add('btnsize');
-        editbtn.classList.add('btnsize');
-        complatebtn.classList.add('btnsize');
-        importntbtn.classList.add('btnsize');
-    
-        taskdis.appendChild(taskdiv);                                         
-        taskdiv.appendChild(taskpera);    
-        taskdiv.appendChild(editbtn);
-        taskdiv.appendChild(importntbtn);
-        taskdiv.appendChild(complatebtn);
-        taskdiv.appendChild(delbtn);
-        
-        
-        delbtn.innerText = 'delete';
-        editbtn.innerText = 'edit';
-        complatebtn.innerText = 'complate';
-        importntbtn.innerText = 'imp';
-    
-        taskpera.setAttribute('data-id', element.id);
-    
-        taskpera.innerText = element.task;
+    taskdis.appendChild(taskdiv);
+    taskdiv.appendChild(taskpera);
+    taskdiv.appendChild(editbtn);
+    taskdiv.appendChild(importntbtn);
+    taskdiv.appendChild(complatebtn);
+    taskdiv.appendChild(delbtn);
 
-        if (element.import === true) {
-            taskpera.classList.add("important");
-        }
+    delbtn.innerText = "delete";
+    editbtn.innerText = "edit";
+    complatebtn.innerText = "complate";
+    importntbtn.innerText = "imp";
 
-        if(element.complatetask){
-            taskpera.style.textDecoration = "line-through";
-        }
-        
-        let uncomlatetask = totaltask.filter(task => task.complatetask === false);
-        panding.innerText = `Panding task is ${uncomlatetask.length}`;
+    taskpera.setAttribute("data-id", element.id);
 
-        let complate = totaltask.filter(task => task.complatetask === true);
-        complaTeTask.innerText = `complated task is ${complate.length}`;
+    taskpera.innerText = element.task;
 
-        let imptask = totaltask.filter(task=>task.import === true);
-        impTasktext.innerText = `total important task is ${imptask.length}`;
+    if (element.import === true) {
+      taskpera.classList.add("important");
+    }
 
-    });
-})
+    if (element.complatetask) {
+      taskpera.style.textDecoration = "line-through";
+    }
+
+    let uncomlatetask = totaltask.filter((task) => task.complatetask === false);
+    panding.innerText = `Panding task is ${uncomlatetask.length}`;
+
+    let complate = totaltask.filter((task) => task.complatetask === true);
+    complaTeTask.innerText = `complated task is ${complate.length}`;
+
+    let imptask = totaltask.filter((task) => task.import === true);
+    impTasktext.innerText = `total important task is ${imptask.length}`;
+  });
+});
 
 taskbtn.addEventListener("click", taskadd);
-todolist.addEventListener('click',mainfunction);
+todolist.addEventListener("click", mainfunction);
 
-srachBtn.addEventListener('keyup',()=>{
-    detail.innerHTML = "";
-    
-    let findTask = srachBtn.value;
-    
-    const findfilter = totaltask.filter(task => task.task.toLowerCase().includes(findTask));
+srachBtn.addEventListener("keyup", () => {
+  detail.innerHTML = "";
 
-    findfilter.forEach((element)=>{
-        const taskpera = document.createElement('li');
-        const taskdiv  = document.createElement('div');
-        const editbtn = document.createElement('button');
-        const delbtn = document.createElement('button');
-        const complatebtn = document.createElement('button');
-        const importntbtn = document.createElement('button');
-    
-        taskdiv.classList.add('pera');
-        taskpera.classList.add('p');
-        delbtn.classList.add('btnsize');
-        editbtn.classList.add('btnsize');
-        complatebtn.classList.add('btnsize');
-        importntbtn.classList.add('btnsize');
-    
-        taskdis.appendChild(taskdiv);                                         
-        taskdiv.appendChild(taskpera);    
-        taskdiv.appendChild(editbtn);
-        taskdiv.appendChild(importntbtn);
-        taskdiv.appendChild(complatebtn);
-        taskdiv.appendChild(delbtn);
-        
-        
-        delbtn.innerText = 'delete';
-        editbtn.innerText = 'edit';
-        complatebtn.innerText = 'complate';
-        importntbtn.innerText = 'imp';
-    
-        taskpera.setAttribute('data-id', element.id);
-    
-        taskpera.innerText = element.task;
-    })
+  let findTask = srachBtn.value;
 
-})
+  const findfilter = totaltask.filter((task) =>
+    task.task.toLowerCase().includes(findTask)
+  );
 
-export{taskbtn ,totaltask};
+  findfilter.forEach((element) => {
+    const taskpera = document.createElement("li");
+    const taskdiv = document.createElement("div");
+    const editbtn = document.createElement("button");
+    const delbtn = document.createElement("button");
+    const complatebtn = document.createElement("button");
+    const importntbtn = document.createElement("button");
+
+    taskdiv.classList.add("pera");
+    taskpera.classList.add("p");
+    delbtn.classList.add("btnsize");
+    editbtn.classList.add("btnsize");
+    complatebtn.classList.add("btnsize");
+    importntbtn.classList.add("btnsize");
+
+    taskdis.appendChild(taskdiv);
+    taskdiv.appendChild(taskpera);
+    taskdiv.appendChild(editbtn);
+    taskdiv.appendChild(importntbtn);
+    taskdiv.appendChild(complatebtn);
+    taskdiv.appendChild(delbtn);
+
+    delbtn.innerText = "delete";
+    editbtn.innerText = "edit";
+    complatebtn.innerText = "complate";
+    importntbtn.innerText = "imp";
+
+    taskpera.setAttribute("data-id", element.id);
+
+    taskpera.innerText = element.task;
+  });
+});
+
+export { taskbtn, totaltask };
